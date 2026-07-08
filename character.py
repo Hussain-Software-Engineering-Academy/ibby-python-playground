ba0ec63cb53a0ccd9088ce7a91e38c363439da93
Ibraheem Hussain <Ibby.hussain914@gmail.com>
2026-07-08 11:38:51 -0400

    Boss Battle Day 3 Test

---
+++ character.py
@@ -0,0 +1,14 @@
+print ("=== Character Stats ===")
+print ()
+print ()
+name = "Ibby"
+health = 100
+gold = 50
+weapon = "Wooden Sword"
+print (f"Name: {name}")
+print (f"Health: {health}")
+print (f"Gold: {gold}")
+print (f"Weapon: {weapon}")
+print ()
+print ()
+print ("Good luck on your adventure!")
